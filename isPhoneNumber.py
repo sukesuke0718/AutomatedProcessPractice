a90f5085ca1b17ead7cfa78ddd24915cf8c423fc
ユウ <sukesuke0718@gmail.com>
2018-10-13 02:18:55 +0900

    新規作成

---
+++ isPhoneNumber.py
@@ -0,0 +1,25 @@
+################################
+# 電話番号かどうかを判定
+################################
+
+def is_phone_number(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+
+print(is_phone_number('415-555-3233'))
+print(is_phone_number('momo'))
