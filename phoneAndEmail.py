7068a93ca7efa3a0c64af0415d422d5c89fe8ce8
ユウ <sukesuke0718@gmail.com>
2018-10-21 23:16:21 +0900

    メアドの正規表現を追加しました

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -3,6 +3,7 @@
 
 import pyperclip, re
 
+# 米国の場合
 phone_regex = re.compile(r'''(
                         (\d{3}|\(\d{3}\))?                         # 市外局番
                         (\s|-|\.)?                                 # 区切り
@@ -14,6 +15,12 @@ phone_regex = re.compile(r'''(
                 )''', re.VERBOSE)
 
 # TODO: 電子メールの正規表現を作る
+email_regex = re.compile(r'''(
+                        [a-zA-Z0-9._%+-]+       # ユーザ名
+                        @                        # @記号
+                        [a-zA-Z0-9.]+           # ドメイン名
+                        (\.[a-zA-Z]{2, 4})      # ドットなんとか
+                      )''', re.VERBOSE)
 
 # TODO: クリップボードのテキストを検索する。
 
