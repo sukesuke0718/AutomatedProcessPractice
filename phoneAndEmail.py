53c5a0e867655570dee95d8668d3dec728daedfa
ユウ <sukesuke0718@gmail.com>
2018-10-21 23:37:34 +0900

    クリップボードに張り付ける処理を追加しました

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -10,8 +10,7 @@ phone_regex = re.compile(r'''(
                         (\d{3})                                    # 3桁の番号
                         (\s|-|\.)                                 # 区切り
                          (\d{4})                                  # 4桁の番号
-                         (\s*(ext|x|ext.)\s*(\d{2, 5}))         # 内線番号
-                        
+                         (\s*(ext|x|ext.)\s*(\d{2,5}))?         # 内線番号
                 )''', re.VERBOSE)
 
 # TODO: 電子メールの正規表現を作る
@@ -19,19 +18,24 @@ email_regex = re.compile(r'''(
                         [a-zA-Z0-9._%+-]+       # ユーザ名
                         @                        # @記号
                         [a-zA-Z0-9.]+           # ドメイン名
-                        (\.[a-zA-Z]{2, 4})      # ドットなんとか
+                        (\.[a-zA-Z]{2,4})      # ドットなんとか
                       )''', re.VERBOSE)
 
 # TODO: クリップボードのテキストを検索する。
 text = str(pyperclip.paste())
 mathes = []
 for groups in phone_regex.findall(text):
-    phone_num = '_'.join([groups[1], groups[3], groups[5]])
+    phone_num = '-'.join([groups[1], groups[3], groups[5]])
     if groups[8] != '':
         phone_num += ' x' + groups[8]
     mathes.append(phone_num)
 for groups in email_regex.findall(text):
     mathes.append(groups[0])
 
-
 # TODO: 検索結果をクリップボードに張り付ける。
+if len(mathes) > 0:
+    pyperclip.copy('\n'.join(mathes))
+    print("クリップボードにコピーしました")
+    print('\n'.join(mathes))
+else:
+    print("電話番号やメールアドレスは見つかりませんでした")
