84821f115ca4dce5cc7b51223c096a07dd9d7ffa
ユウ <sukesuke0718@gmail.com>
2018-11-11 22:48:59 +0900

    zipファイル作成処理を追加

--- backupToZip.py
+++ backupToZip.py
@@ -10,12 +10,16 @@ def backup_to_zip(folder):
     # 既存のファイル名からファイル名の連番を決める
     number = 1
     while True:
+        # 新しくつけるファイルの名前
         zip_filename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        # ファイルが見つからなかったらループを抜ける
         if not os.path.exists(zip_filename):
             break
         number = number + 1
 
-    # TODO: ZIPファイルを作成する
+    # ZIPファイルを作成する
+    print('Creating {} ...'.format(zip_filename))
+    backup_zip = zipfile.ZipFile(zip_filename, 'w')
 
     # TODO; フォルダーのツリーを渡り歩いてその中のファイルを圧縮する
     print('Done.')
